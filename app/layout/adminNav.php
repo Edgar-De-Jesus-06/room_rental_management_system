9d22c1ca515ea682fb4d95478da194755d4a85c1
Edgar-De-Jesus-06 <edgarb.dejesus@gmail.com>
2026-07-03 18:28:33 +0800

    adjust the width of navigation

--- app/layout/adminNav.php
+++ app/layout/adminNav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg border position-absolute top-0 start-0 min-vh-100 d-flex flex-column" style="width: 250px;">
+<nav class="navbar navbar-expand-lg border min-vh-100 d-flex flex-column" style="width: 280px;">
     <div class="container-fluid p-0 d-flex flex-column">
         <div class="container p-3 border-bottom d-flex align-content-center">
             <i class="bi bi-building rounded-3 fs-4 p-1 text-light" style="background-color: #0f2573;"></i>
@@ -9,7 +9,7 @@
         </div>
         <div class="container p-3" style="height: 700px;">
             <a href="./dashboard.php" class="nav-link text-secondary fw-medium p-2 rounded-3">
-                <i class="bi bi-bar-chart me-2"></i>
+                <i class="bi bi-columns-gap me-2"></i>
                 Dashboard
             </a>
             <a href="./rooms.php" class="nav-link text-secondary fw-medium p-2 rounded-3">
