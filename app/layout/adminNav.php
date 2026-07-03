886743f12f4a01626f4f0010b0d26357d3e2f837
Edgar-De-Jesus-06 <edgarb.dejesus@gmail.com>
2026-07-03 13:36:49 +0800

    navigation page

---
+++ app/layout/adminNav.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg border position-absolute top-0 start-0 min-vh-100 d-flex flex-column" style="width: 250px;">
+    <div class="container-fluid p-0 d-flex flex-column">
+        <div class="container p-3 border-bottom d-flex align-content-center">
+            <i class="bi bi-building rounded-3 fs-4 p-1 text-light" style="background-color: #0f2573;"></i>
+            <div class="container">
+                <p class="fw-bolder m-0" style="color: #0f2573;">Room Rental</p>
+                <p class="text-secondary m-0 fw-medium" style="color: #0f2573; font-size: 12px;">Management System</p>
+            </div>
+        </div>
+        <div class="container p-3" style="height: 700px;">
+            <a href="./dashboard.php" class="nav-link text-secondary fw-medium p-2 rounded-3">
+                <i class="bi bi-bar-chart me-2"></i>
+                Dashboard
+            </a>
+            <a href="./rooms.php" class="nav-link text-secondary fw-medium p-2 rounded-3">
+                <i class="bi bi-building me-2"></i>
+                Rooms
+            </a>
+        </div>
+    </div>
+    <div class="container border-top p-3">
+        <button type="button" class="btn text-light w-100 fw-medium" style="background-color: #0f2573;">
+            Log out
+        </button>
+    </div>
+</nav>
