6ee2614b81481e70f58b7797559509901585b173
Edgar-De-Jesus-06 <edgarb.dejesus@gmail.com>
2026-07-19 15:00:25 +0800

    added rest api then later i will fix it

--- app/src/controller/api/admin/EditRoom.php
+++ app/src/controller/api/admin/EditRoom.php
@@ -4,53 +4,59 @@
 
     header("Content-Type: application/json; Charset=UTF-8");
     header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, POST");
+    header("Access-Control-Allow-Methods: GET, PATCH");
     header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
     use app\model\admin\RoomModel;
 
     $room_model = new RoomModel();
 
-    if($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['save_edit'])) {
-        $edit_id         = $_POST['id'] ?? '';
-        $edit_room_num   = filter_input(INPUT_POST, 'edit_room_num', FILTER_SANITIZE_SPECIAL_CHARS);
-        $edit_type       = filter_input(INPUT_POST, 'edit_type', FILTER_SANITIZE_SPECIAL_CHARS);
-        $edit_floor      = filter_input(INPUT_POST, 'edit_floor', FILTER_VALIDATE_INT);
-        $edit_rent_price = filter_input(INPUT_POST, 'edit_rent_price', FILTER_VALIDATE_INT);
-        $edit_status     = filter_input(INPUT_POST, 'edit_status', FILTER_SANITIZE_SPECIAL_CHARS);
+    if($_SERVER['REQUEST_METHOD'] === "PATCH") {
+        $data = json_decode(file_get_contents('php://input'), true);
 
-        if(empty($edit_id)) {
+        $id              = (int) $data['id'] ?? 0;
+        $edit_room_num   = $data['edit_room_num'] ?? '';
+        $edit_floor      = $data['edit_floor'] ?? '';
+        $edit_type       = $data['edit_type'] ?? '';
+        $edit_rent_price = $data['edit_rent_price'] ?? '';
+        $edit_status     = $data['edit_status'] ?? '';
+
+        if($id == 0) {
             http_response_code(404);
             echo json_encode([
-                'message' => 'ID is Not Found',
+                'message' => 'ID is not found',
                 'status'  => 404
             ]);
             exit();
         }
 
-        if(empty($edit_room_num) || empty($edit_type) || $edit_floor == null || $edit_rent_price == null || empty($edit_status)) {
+        if(empty($edit_room_num) || empty($edit_type) || empty($edit_floor) || empty($edit_rent_price) || empty($edit_status)) {
             http_response_code(400);
             echo json_encode([
-                'message' => 'Bad Request',
+                'message' => 'Empty field, please try again',
                 'status'  => 400
             ]);
             exit();
         }
 
-        $room_model->room_no = $edit_room_num;
-        $room_model->type    = $edit_type;
-        $room_model->floor   = $edit_floor;
-        $room_model->price   = $edit_rent_price;
-        $room_model->status  = $edit_status;
-        
-        http_response_code(200);
-        $room_model->updateRoomsData($edit_id);
-        echo json_encode([
-            'message' => 'Your changes have been saved successfully.',
-            'status'  => 200
-        ]);
-        exit();
-    
+        try {
+            $room_model->room_no = $edit_room_num;
+            $room_model->floor   = $edit_floor;
+            $room_model->type    = $edit_type;
+            $room_model->price   = $edit_rent_price;
+            $room_model->status  = $edit_status;
+
+            http_response_code(200);
+            $room_model->updateRoomsData($id);
+            echo json_encode([
+                'data'   => $data,
+                'status' => 200
+            ]);
+            exit();
+
+        } catch(\PDOException $e) {
+            echo $e->getMessage();
+        }
 
     } elseif($_SERVER['REQUEST_METHOD'] === "GET") {
         $room_id = (int) $_GET['room_id'] ?? 0;
